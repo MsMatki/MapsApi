c94b051a3225d538e81aa8791ce5c2fa24cdb7a6
Bartol Bilankov <bartol.bilankov@gmail.com>
2018-05-25 14:26:33 -0400

    Added files

---
+++ README.md
@@ -0,0 +1,18 @@
+# Neighborhood Map
+
+* This app is to demonstrate the ability to use a js framework knockout.js & Google Maps API.
+
+# Built With
+
+* JavaScript
+* HTML
+* CSS
+* Bootstrap
+* KnockoutJS
+* Google Maps API
+* Foursquare API
+
+# Download
+
+* Download the project or clone the repository
+* Open index.html
---
+++ js/markers.js
@@ -0,0 +1,52 @@
+var myLocations = [
+    {
+        title: 'Westmount Golf & Country Club',
+        lat: 43.4485,
+        lng:  -80.5339,
+    },
+    {
+        title: 'David Johnston Research & Technology Park',
+        lat: 43.4804,
+        lng: -80.5494
+    },
+    {
+        title: 'Wilfrid Laurier University',
+        lat: 43.4724,
+        lng: -80.5263
+    },
+    {
+        title: 'University of Waterloo',
+		lat: 43.4723,
+		lng: -80.5449
+    },
+    {
+		title: 'St. Mary’s General Hospital',
+		lat: 43.4379,
+		lng: -80.5014
+	},
+	{
+		title: 'Grand River Hospital',
+		lat: 43.4563,
+		lng: -80.5122
+	},
+	{
+		title: 'Hospice of Waterloo Region',
+		lat: 43.4415,
+		lng: -80.5129
+	},
+	{
+		title: 'Kitchener-Waterloo Art Gallery',
+		lat: 43.4548,
+		lng: -80.4846
+	},
+    {
+        title: 'Multicultural Theatre Space',
+        lat: 43.4481,
+        lng: -80.4848,
+    },
+    {
+        title: 'Stanley Park Community Centre',
+        lat: 43.4482,
+        lng: -80.4490,
+    }
+]
